8ba2b89c31b1b24b61086f6e4e6df379ab0c8626
Augusto Nunes <augusto.cesar@gmail.com>
2016-06-07 18:09:50 -0300

    avançando...

--- TP4.R
+++ TP4.R
@@ -18,3 +18,57 @@ dados.std.teste <- dados.std[sample.int(nrow(dados.std), 100),]
 vizinhanca <- lapply(distancias.matriz, sort.list)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dados <- read.csv("heart.txt", sep="", header=FALSE)
+
+padronizar <- function(x){
+  return((x - min(x))/(max(x)-min(x)))
+}
+dados.std <- NULL
+
+dados.std <- as.data.frame(lapply(dados, padronizar))
+# NOTAR QUE PADRONIZAMOS INCLUSIVE A CATEGORIA 
+# COLUNA 14, QUE ERA 1-2
+# E PASSOU A SER 0-1
+
+
+dados.std <- data.frame(dados.std)
+colnames(dados.std) <- colnames(dados)
+
+set.seed(13)
+dados.std.validacao <- head(dados.std, n = 100)
+dados.std.treino <- tail(dados.std,n = 170)
+
+knn.ingenuo <- function(treino, validacao, k = 5){
+  for(i in 1:length(validacao)){
+    mat.distancias <- dist(rbind(validacao[i,], treino))
+    minimo <- which(mat.distancias == min(mat.distancias), arr.ind = TRUE)
+  }
+}
